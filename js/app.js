4f26b5863380c509e049315bd929ab7fd54911d9
sofia zaim <sofiazaim@sofias-MacBook-Pro.local>
2016-07-18 17:22:06 -0400

    final work

--- js/app.js
+++ js/app.js
@@ -7,4 +7,29 @@ $(document).ready(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-still').hide();		
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500, 
+			function() {
+				$(this).hide();
+				$(this).css('left', '580px');
+			}
+		);
+	})
+	.mouseup(function() {
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').hide();
+	})
+	function playHadouken(){
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
+
 });
